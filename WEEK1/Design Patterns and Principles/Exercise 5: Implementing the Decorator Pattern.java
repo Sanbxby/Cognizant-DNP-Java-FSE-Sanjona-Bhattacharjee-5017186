61522f87a607da5ec32167d51f1b9f375e7edc39
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-07-29 19:53:55 +0530

    Create Exercise 5: Implementing the Decorator Pattern.java

---
+++ WEEK1/Design Patterns and Principles/Exercise 5: Implementing the Decorator Pattern.java
@@ -0,0 +1,44 @@
+interface Notifier {
+    void send(String message);
+}
+
+class EmailNotifier implements Notifier {
+    @Override
+    public void send(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+abstract class NotifierDecorator implements Notifier {
+    protected Notifier notifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    public void send(String message) {
+        notifier.send(message);
+    }
+}
+
+class SMSNotifierDecorator extends NotifierDecorator {
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+public class DecoratorPatternExample {
+    public static void main(String[] args) {
+        Notifier emailNotifier = new EmailNotifier();
+        emailNotifier.send("Hieeeee");
+
+        Notifier smsNotifier = new SMSNotifierDecorator(new EmailNotifier());
+        smsNotifier.send("Hi from SMS and email");
+    }
+}
