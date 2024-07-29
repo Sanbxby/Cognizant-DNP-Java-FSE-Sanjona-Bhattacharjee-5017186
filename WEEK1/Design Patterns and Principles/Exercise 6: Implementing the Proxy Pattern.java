14a9eba42a558034527ee0a3683c8f72e09f75a2
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-07-29 20:06:40 +0530

    Create Exercise 6: Implementing the Proxy Pattern.java

---
+++ WEEK1/Design Patterns and Principles/Exercise 6: Implementing the Proxy Pattern.java
@@ -0,0 +1,60 @@
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk();
+    }
+
+    private void loadFromDisk() {
+        System.out.println("Loading " + fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+}
+
+class ProxyImage implements Image {
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
+
+public class ProxyPatternExample {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("photo1.jpg");
+        Image image2 = new ProxyImage("photo2.jpg");
+
+    
+        image1.display();
+        System.out.println("");
+
+        
+        image1.display();
+        System.out.println("");
+
+    
+        image2.display();
+        System.out.println("");
+
+    
+        image2.display();
+    }
+}
