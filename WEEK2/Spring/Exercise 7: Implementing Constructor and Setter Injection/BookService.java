af05d21a26a70b26c0b8e031e81d02f7d59a15aa
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-05 18:59:59 +0530

    Create BookService.java

---
+++ WEEK2/Spring/Exercise 7: Implementing Constructor and Setter Injection/BookService.java
@@ -0,0 +1,23 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor for dependency injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Setter for dependency injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void performService() {
+        System.out.println("Performing book service operation...");
+        bookRepository.findAllBooks();
+    }
+}
