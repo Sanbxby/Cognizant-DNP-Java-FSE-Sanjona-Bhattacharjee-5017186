42919d11465986ade40495cb2f7f51c047e5f7e9
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-05 18:37:58 +0530

    Create LoggingAspect.java

---
+++ WEEK2/Spring/Exercise 3: Implementing Logging with Spring AOP/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        logger.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
+        return proceed;
+    }
+}
