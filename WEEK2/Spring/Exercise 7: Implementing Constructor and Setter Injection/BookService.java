ed196d318091b4c12368cb1a05b28d330baa8a48
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-05 20:31:55 +0530

    Update BookService.java

--- WEEK2/Spring/Exercise 7: Implementing Constructor and Setter Injection/BookService.java
+++ WEEK2/Spring/Exercise 7: Implementing Constructor and Setter Injection/BookService.java
@@ -6,12 +6,12 @@ public class BookService {
 
     private BookRepository bookRepository;
 
-    // Constructor for dependency injection
+    
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    // Setter for dependency injection
+    
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
