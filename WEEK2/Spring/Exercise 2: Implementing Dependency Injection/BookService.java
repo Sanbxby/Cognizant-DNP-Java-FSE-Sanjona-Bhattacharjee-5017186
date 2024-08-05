ff1288ef0a30f1edfc93d03c596ee5f2fa6c834e
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-05 20:30:03 +0530

    Update BookService.java

--- WEEK2/Spring/Exercise 2: Implementing Dependency Injection/BookService.java
+++ WEEK2/Spring/Exercise 2: Implementing Dependency Injection/BookService.java
@@ -6,7 +6,7 @@ public class BookService {
 
     private BookRepository bookRepository;
 
-    // Setter for dependency injection
+    
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
