ecf49d7fc651ab8e581f1c153753a2d3df2efc7b
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-13 11:43:56 +0530

    Create EmployeeController.java

---
+++ WEEK3/Exercise 4: Employee Management System - Implementing CRUD Operations/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.employeemanagementsystem.controller;
+import com.example.employeemanagementsystem.entity.Employee;
+import com.example.employeemanagementsystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
