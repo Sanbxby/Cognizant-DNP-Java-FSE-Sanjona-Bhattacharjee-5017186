34b186dd5292ed93a72d33404a4a8f47b8c395da
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-13 12:03:36 +0530

    Create EmployeeServiceImpl.java

---
+++ WEEK3/Exercise 6: Employee Management System - Implementing Pagination and Sorting/Exercise 6: Employee Management System - Implementing Pagination and Sorting/EmployeeServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.employeemanagementsystem.service;
+
+import com.example.employeemanagementsystem.entity.Employee;
+import com.example.employeemanagementsystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Page<Employee> getEmployeesWithPagination(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return employeeRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Employee> getEmployeesWithPaginationAndSorting(int page, int size, String sortBy, String sortDirection) {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return employeeRepository.findAll(pageable);
+    }
+}
