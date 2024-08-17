364833c58d6f6f638b33cf4c2c392bd3d743ba71
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-17 19:16:02 +0530

    Create Book.java

---
+++ WEEK4,5/Exercise 2: Online Bookstore - Creating Basic REST Controllers/Book.java
@@ -0,0 +1,16 @@
+package com.example.bookstore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private double price;
+    private String isbn;
+}
