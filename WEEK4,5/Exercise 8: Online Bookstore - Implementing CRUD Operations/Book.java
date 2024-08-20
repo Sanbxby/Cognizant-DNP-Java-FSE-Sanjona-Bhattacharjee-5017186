453e99bbfa514a31548cd0802fbd55d2d5b5e072
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-20 22:19:11 +0530

    Create Book.java

---
+++ WEEK4,5/Exercise 8: Online Bookstore - Implementing CRUD Operations/Book.java
@@ -0,0 +1,27 @@
+
+
+package com.example.bookstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private double price;
+    private String isbn;
+
+    @Version
+    private Integer version;
+
+    
+}
