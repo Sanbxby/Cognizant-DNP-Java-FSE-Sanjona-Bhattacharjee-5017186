2aab2dc870069a7584e97fda791d048961ed5fc5
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-20 23:59:43 +0530

    Create UserDetailsServiceImpl.java

---
+++ WEEK4,5/Exercise 12: Online Bookstore - Securing RESTful Endpoints with Spring Security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+
+
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.User;
+import com.example.bookstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRoles())
+                .build();
+    }
+}
