cf94e6d2e9530b8c1f27b81f4341dde0d9b539aa
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-20 22:31:29 +0530

    Create WebConfig.java

---
+++ WEEK4,5/Exercise 10: Online Bookstore - Configuring Content Negotiation/WebConfig.java
@@ -0,0 +1,19 @@
+// File: src/main/java/com/example/bookstore/config/WebConfig.java
+
+package com.example.bookstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+            .defaultContentType(MediaType.APPLICATION_JSON) // Default to JSON
+            .mediaType("xml", MediaType.APPLICATION_XML); // Support XML
+    }
+}
