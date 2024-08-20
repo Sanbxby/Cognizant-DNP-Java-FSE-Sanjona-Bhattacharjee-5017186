1a3e84c9d30e0ed3ebb2bb47e7d093707f0d76fe
Sanjona Bhattacharjee <sanju.bae19@gmail.com>
2024-08-20 22:25:58 +0530

    Create BookDTO.java

---
+++ WEEK4,5/Exercise 9: Online Bookstore - Understanding HATEOAS/BookDTO.java
@@ -0,0 +1,70 @@
+
+
+package com.example.bookstore.dto;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class BookDTO extends RepresentationModel<BookDTO> {
+
+    private Long id;
+
+    @NotNull(message = "Title cannot be null")
+    @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters")
+    private String title;
+
+    @NotNull(message = "Author cannot be null")
+    @Size(min = 2, max = 100, message = "Author must be between 2 and 100 characters")
+    private String author;
+
+    @Min(value = 0, message = "Price must be positive")
+    private double price;
+
+    @NotNull(message = "ISBN cannot be null")
+    @Size(min = 10, max = 13, message = "ISBN must be between 10 and 13 characters")
+    private String isbn;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
